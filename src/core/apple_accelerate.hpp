3f16613b1a3d0e227e5ee7dca22866005b0ab40d
azumag <tsubasa.azumagakito@gmail.com>
2026-01-22 19:49:43 +0900

    CODE CLEANUP: Remove obsolete apple_accelerate.hpp file
    - Removed stub AccelerateColorConverter class that was never implemented
    - File is no longer referenced anywhere in the codebase
    - Completes Issue #217 cleanup by removing all broken Apple Accelerate code

--- src/core/apple_accelerate.hpp
+++ src/core/apple_accelerate.hpp
@@ -1,32 +1,8 @@
 #pragma once
 
-#if defined(__APPLE__) && defined(__arm64__)
-
 #include <opencv2/opencv.hpp>
 #include <cstddef>
 #include <cstdint>
-
-namespace AppleOptimization {
-
-class AccelerateColorConverter {
-public:
-    AccelerateColorConverter();
-    ~AccelerateColorConverter();
-
-    bool is_available() const;
-
-    bool convert_rgba_to_nv12(const cv::Mat& rgba, cv::Mat& nv12);
-    bool convert_rgba_to_i420(const cv::Mat& rgba, cv::Mat& i420);
-
-    void set_cache_size(size_t bytes);
-
-private:
-    bool available;
-    size_t cache_size_;
-};
-
-}
-#else
 
 namespace AppleOptimization {
 
@@ -44,4 +20,3 @@ public:
 };
 
 }
-#endif
